e5ff478362bb44391ccccc74525f9812ce52e972
dominikus1993 <dominikus1910@gmail.com>
2016-04-25 21:41:36 +0200

    add ICommentsRepository

--- src/main/kotlin/com/devmikroblog/repositories/interfaces/ICommentsRepository.kt
+++ src/main/kotlin/com/devmikroblog/repositories/interfaces/ICommentsRepository.kt
@@ -1,10 +1,15 @@
 package com.devmikroblog.repositories.interfaces
 
 import com.devmikroblog.model.Post
+import java.util.function.Predicate
 
 /**
  * Created by dominik on 25.04.16.
  */
 interface ICommentsRepository {
-
+    fun read():List<Post>?;
+    fun read(predicate: Predicate<Post>):Post?;
+    fun create(comment: Post?, parent:Post?):Boolean;
+    fun update(comments: Post?):Boolean;
+    fun delete(comment: Post?):Boolean;
 }
