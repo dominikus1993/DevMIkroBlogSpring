34c522de64661382926b147ea9843c3c9752d41e
dominikus1993 <dominikus1910@gmail.com>
2016-04-26 20:26:40 +0200

    add CommentsRepository

---
+++ src/main/kotlin/com/devmikroblog/repositories/implementations/CommentsRepository.kt
@@ -0,0 +1,40 @@
+package com.devmikroblog.repositories.implementations
+
+import com.devmikroblog.model.Post
+import com.devmikroblog.repositories.BaseRepository
+import com.devmikroblog.repositories.interfaces.ICommentsRepository
+import org.hibernate.SessionFactory
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.function.Predicate
+
+/**
+ * Created by dominik on 26.04.16.
+ */
+class CommentsRepository : BaseRepository ,ICommentsRepository {
+
+    @Autowired
+    constructor(sessionFactory: SessionFactory) : super(sessionFactory){
+
+    }
+
+    override fun read(): List<Post>? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun read(predicate: Predicate<Post>): Post? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun create(comment: Post?, parent: Post?): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+    override fun update(comments: Post?): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+    override fun delete(comment: Post?): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+}
