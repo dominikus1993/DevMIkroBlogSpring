52e0fc263dec7f28c05929ed04dfd28e4b8e595c
dominikus1993 <dominikus1910@gmail.com>
2016-05-03 17:57:48 +0200

    added UserService implementation

---
+++ src/main/kotlin/com/devmikroblog/services/implementations/UserService.kt
@@ -0,0 +1,43 @@
+package com.devmikroblog.services.implementations
+
+import com.devmikroblog.model.Result
+import com.devmikroblog.model.Role
+import com.devmikroblog.model.User
+import com.devmikroblog.repositories.interfaces.IUserRepository
+import com.devmikroblog.services.interfaces.IUserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+/**
+ * Created by dominik on 03.05.16.
+ */
+@Service
+public class UserService : IUserService{
+
+    private val userRepository: IUserRepository;
+
+    @Autowired
+    constructor(userRepository: IUserRepository) {
+        this.userRepository = userRepository
+    }
+
+    override fun login(username: String, password: String): Result<User?> {
+        throw UnsupportedOperationException()
+    }
+
+    override fun register(user: User): Result<Boolean> {
+        throw UnsupportedOperationException()
+    }
+
+    override fun isAdmin(userId: Int): Result<Boolean> {
+        throw UnsupportedOperationException()
+    }
+
+    override fun changeRole(userId: Int, role: Role): Result<Boolean> {
+        throw UnsupportedOperationException()
+    }
+
+    override fun isOwner(postId: Int, userId: Int): Result<Boolean> {
+        throw UnsupportedOperationException()
+    }
+}
--- src/main/kotlin/com/devmikroblog/services/interfaces/IUserService.kt
+++ src/main/kotlin/com/devmikroblog/services/interfaces/IUserService.kt
@@ -1,5 +1,6 @@
 package com.devmikroblog.services.interfaces
 
+import com.devmikroblog.model.Result
 import com.devmikroblog.model.Role
 import com.devmikroblog.model.User
 
@@ -7,9 +8,9 @@ import com.devmikroblog.model.User
  * Created by dominik on 03.05.16.
  */
 interface IUserService {
-    fun login(username:String, password:String): User?
-    fun register(user: User):Boolean
-    fun isAdmin(userId:Int):Boolean
-    fun changeRole(userId: Int, role: Role):Boolean
-    fun isOwner(postId: Int, userId: Int): Boolean
+    fun login(username:String, password:String): Result<User?>
+    fun register(user: User):Result<Boolean>
+    fun isAdmin(userId:Int):Result<Boolean>
+    fun changeRole(userId: Int, role: Role): Result<Boolean>
+    fun isOwner(postId: Int, userId: Int): Result<Boolean>
 }
