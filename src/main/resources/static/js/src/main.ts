6e3d4ada8375267ef0e08be380213ee9fb896ab0
dominikus1993 <dominikus1910@gmail.com>
2016-05-03 15:47:09 +0200

    add post controller to main view

--- src/main/resources/static/js/src/main.ts
+++ src/main/resources/static/js/src/main.ts
@@ -1,6 +1,12 @@
 ///<reference path="tsd.d.ts"/>
 ///<reference path="homeController.ts"/>
+///<reference path="postController.ts"/>
+///<reference path="postService.ts"/>
+
 
 const appModule = angular.module("devmikroblog", []);
 appModule.controller("HomeController", ["$rootScope", "$scope", ($rootScope, $scope) => new Controllers.HomeController($rootScope, $scope)]);
+appModule.controller("PostController", ["$rootScope", "$scope", "PostService", ($rootScope, $scope, postService) => new Controllers.PostController($rootScope, $scope, postService)]);
 appModule.controller("UserController", ["$rootScope", "$scope"]);
+
+appModule.factory("PostService", ($http) => new Services.PostService($http));
--- src/main/resources/static/js/src/postController.ts
+++ src/main/resources/static/js/src/postController.ts
@@ -4,8 +4,19 @@
 module Controllers{
     import IPostService = Services.IPostService;
     export class PostController{
+
+        public posts: Model.Post[];
+
         constructor(private rootSocpe:ng.IRootElementService, private scope:ng.IScope, private postService:IPostService){
 
+        }
+
+        public getAll(){
+            this.postService.getAll((data: Model.Result<Model.Post[]>) => {
+                if(data.success){
+                    this.posts = data.value.reverse();
+                }
+            });
         }
     }
 }
