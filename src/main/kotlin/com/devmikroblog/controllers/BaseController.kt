1bc8279a9e200c63955a3855820e1cd8ad577240
dominikus1993 <dominikus1910@gmail.com>
2016-05-17 19:03:15 +0200

    add isOwner method to BaseController

--- src/main/kotlin/com/devmikroblog/controllers/BaseController.kt
+++ src/main/kotlin/com/devmikroblog/controllers/BaseController.kt
@@ -1,5 +1,6 @@
 package com.devmikroblog.controllers
 
+import com.devmikroblog.model.Result
 import com.devmikroblog.services.interfaces.IUserService
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -9,4 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired
 open class BaseController{
     @Autowired
     protected lateinit var userService: IUserService;
+
+    protected fun isOwner(postId:Int, userId : Int) : Result<Boolean> {
+        return userService.isOwner(postId, userId)
+    }
 }
